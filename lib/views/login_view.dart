71cd4a79ba38ce24d1687d278464078a3d291b74
Karl Marx Roxas <karlm.roxas@gmail.com>
2025-02-07 11:40:35 +0800

    step-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -67,14 +68,10 @@ class _LoginViewState extends State<LoginView> {
 
                 Navigator.of(context)
                     .pushNamedAndRemoveUntil(notesRoute, (route) => false);
-                    
               } on FirebaseAuthException catch (e) {
-                if (e.code == 'user-not-found') {
-                  devtools.log('No user found for that email.');
-                } else {
-                  devtools.log("SOMETHING WENT WRONG");
-                  devtools.log(e.code);
-                }
+                await showErrorDialog(context, 'Error: ${e.code}');
+              } catch (e) {
+                await showErrorDialog(context, 'Error: ${e.toString()}');
               }
             },
             child: const Text("Login"),
